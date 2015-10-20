87ff4efca6b6ae9f096bfc6d4021d0b08e0f66ec
Oskar Risberg <kokarn@gmail.com>
2015-10-20 10:12:57 +0200

    Initial list with some comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # web-vuln-scan-list
-A list of paths scanned by automated systems to find vulnerabilities on a system
+A list of paths or partial paths scanned by automated systems to find vulnerabilities on a system.
+
+Easy to use to reduce noise when checking for errors in access logs.
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = [
+    'testproxy.php', // Proxy finder
+    'tmUnblock.cgi',
+    'getheaders9.php',
+    'getheaders2.php',
+    'cgi-bin/common/attr',
+    'scripts/setup.php', // phpMyAdmin setup path
+    'muieblackcat' // Muieblackcat http://eromang.zataz.com/2011/08/14/suc027-muieblackcat-setup-php-web-scanner-robot/
+];

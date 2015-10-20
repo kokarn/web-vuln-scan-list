76bf397a83432921653d488b77f74b8ae7dd7909
Oskar Risberg <kokarn@gmail.com>
2015-10-20 11:24:21 +0200

    Add better explanation to some paths

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = [
-    'testproxy.php', // Proxy finder
+    'testproxy.php', // open proxy test
     'tmUnblock.cgi',
     'getheaders9.php',
     'getheaders2.php',
-    'cgi-bin/common/attr',
+    'cgi-bin/common/attr', // open proxy test
     'scripts/setup.php', // phpMyAdmin setup path
     'muieblackcat' // Muieblackcat http://eromang.zataz.com/2011/08/14/suc027-muieblackcat-setup-php-web-scanner-robot/
 ];

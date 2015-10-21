d57d422513fb07ecac9deaa4d6c8cea9db345f59
Oskar Risberg <kokarn@gmail.com>
2015-10-21 11:44:03 +0200

    Sort alphabetically to avoid duplicates

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 'use strict';
 
 module.exports = [
-    'webmap.cgi',
-    'helpdesk.cgi',
-    'info.cgi',
-    'administrator.cgi',
-    'index.cgi',
     'admin.cgi',
-    'whois.cgi',
-    'report.cgi',
-    'main.cgi',
-    'index2.cgi',
-    'reboot.cgi',
-    'firewall.cgi',
-    'kontakt.cgi',
-    'query.cgi',
-    'concept.cgi',
-    'cgi-bin/printenv',
+    'administrator.cgi',
     'backup.cgi',
+    'cgi-bin/common/attr', // open proxy test
+    'cgi-bin/printenv',
+    'concept.cgi',
+    'count.cgi',
     'counter.cgi',
+    'firewall.cgi',
+    'formmail.cgi',
+    'getheaders2.php',
+    'getheaders9.php',
+    'helpdesk.cgi',
+    'index.cgi',
+    'index2.cgi',
+    'info.cgi',
+    'kontakt.cgi',
+    'main.cgi',
+    'muieblackcat', // Muieblackcat http://eromang.zataz.com/2011/08/14/suc027-muieblackcat-setup-php-web-scanner-robot/
+    'query.cgi',
+    'reboot.cgi',
+    'report.cgi',
+    'scripts/setup.php', // phpMyAdmin setup path
     'supply.cgi',
     'test.sh',
-    'formmail.cgi',
-    'count.cgi',
     'testproxy.php', // open proxy test
     'tmunblock.cgi',
-    'getheaders9.php',
-    'getheaders2.php',
-    'cgi-bin/common/attr', // open proxy test
-    'scripts/setup.php', // phpMyAdmin setup path
-    'muieblackcat' // Muieblackcat http://eromang.zataz.com/2011/08/14/suc027-muieblackcat-setup-php-web-scanner-robot/
+    'webmap.cgi',
+    'whois.cgi'
 ];
